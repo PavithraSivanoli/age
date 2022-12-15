1a2b7783615ed92345e11a8f8b6f6da9730953b0
unknown <pavithrasivanoli@gmail.com>
2022-12-15 21:22:06 +0530

    adding new files

---
+++ js/main.js
@@ -0,0 +1,123 @@
+var a=parseInt("enter a age");
+		var b=parseInt("enter b age");
+		var c=parseInt("enter c age");
+		if (a>b)
+		{
+			console.log("a is greater")
+		}
+		else
+		{
+			console.log("not greater ")
+		}
+		 if (b>a)
+			{
+				console.log("b is greater")
+			}
+			else 
+			{
+				console.log("not greater")
+			}
+				
+			if (c>a)
+			{
+				console.log("c is greater")
+			}
+			else 
+			{
+				console.log("not greater")
+			}
+			
+				
+/*var age= parseInt(prompt("enter the age "));
+var gender=prompt("enter the gender");
+
+if(gender == 'Female')
+{
+	alert("work in urban areas" )
+}
+
+else if((age >= 20) && (age < 40)&&(gender == 'male')) 
+{
+	alert("allow to  work anywhere")
+			
+	}
+else if((age >= 40) && (age < 60)&& (gender == 'male'))) 
+{
+   alert(" allow to work  in urban areas")
+}		
+else {
+				
+	alert("not allowed")
+}*/
+/*var a= 5;
+var b=6;
+var c= 7;
+if (a<b && a<c)
+{
+        if (b<c)
+        {
+          alert(a+ " " +b " "+c);
+        }
+        else if
+        {
+            alert(a+" "+c " " +b);
+        }
+}
+else if (b<a && b<c)
+{
+        if (a<c)
+        {
+             alert(b+ " " +a " " +c);
+        }
+        else
+        {
+             alert(b+ " " +c " " +a);
+        }
+}
+else if (c<a && c>b)
+{
+        if (a>b)
+        {
+            alert(c+ " " +a " " +b);
+        }
+        else
+        {
+            alert(c+ " " +a " " +b);
+        }
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
